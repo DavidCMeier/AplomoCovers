6e013c1889ca017f6c3b9778bbde064601e13993
David Calvo Meier <davidcalvomeier@protonmail.com>
2025-05-29 15:26:34 +0200

    Actualizar concerts.ts

--- src/data/concerts.ts
+++ src/data/concerts.ts
@@ -84,6 +84,13 @@ export const concerts: Concert[] = [
     "ticketsAvailable": false
   },
   {
+    "date": "2025-06-21",
+    "venue": "Balsa de Cela",
+    "map": "https://maps.app.goo.gl/pNdVT9SrsJH4gRHd6",
+    "city": "Lucar, Almería",
+    "ticketsAvailable": false
+  },
+  {
     "date": "2025-06-28",
     "venue": "Pub Principal",
     "map": "https://maps.app.goo.gl/8KpjbcTjo9ttzzyk6",
